b2d29eece5054bc5ffb2d6536199024400b71184
Milton Llera <lleramontero@gmail.com>
2018-11-18 23:55:06 +0000

    Fixed error in job-script.

--- benchmarks/seqMNIST/slurm-jobs/parallel-comparison.sh
+++ benchmarks/seqMNIST/slurm-jobs/parallel-comparison.sh
@@ -9,31 +9,31 @@
 #SBATCH --time=10:0:0
 #SBATCH --mem=2500M
 
-module add languages/anaconda3/5.0.2-tflow-1.11
+module add languages/anaconda3/5.2.0-tflow-1.11
 module add libs/cuda/9.0-gcc-5.4.0-2.26
 module add libs/cudnn/9.0-cuda-9.0
 
 echo 'Comparing LSTM variants on sequential MNIST'
 
-srun python benchmarks/seqMNIST/main.py  \
-    --model subLSMT --nlayers 1 --nhid 50 
+srun python benchmarks/seqMNIST/run.py  \
+    --model subLSTM --nlayers 1 --nhid 50 
     --lr 10e-4 --optim rmsrop --epochs 40 --batch-size 200 \
     --seed 18092 --cuda --log-interval 200 \
     --save ./benchmarks/seqMNIST/results
 
-srun python benchmarks/seqMNIST/main.py  \
-    --model fix-subLSMT --nlayers 1 --nhid 50 
+srun python benchmarks/seqMNIST/run.py  \
+    --model fix-subLSTM --nlayers 1 --nhid 50 
     --lr 10e-4 --optim rmsrop --epochs 40 --batch-size 200 \
     --seed 18092 --cuda --log-interval 200 \
     --save ./benchmarks/seqMNIST/results
 
-srun python benchmarks/seqMNIST/main.py  \
-    --model LSMT --nlayers 1 --nhid 50 
+srun python benchmarks/seqMNIST/run.py  \
+    --model LSTM --nlayers 1 --nhid 50 
     --lr 10e-4 --optim rmsrop --epochs 40 --batch-size 200 \
     --seed 18092 --cuda --log-interval 200 \
     --save ./benchmarks/seqMNIST/results
 
-srun python benchmarks/seqMNIST/main.py  \
+srun python benchmarks/seqMNIST/run.py  \
     --model GRU --nlayers 1 --nhid 50 
     --lr 10e-4 --optim rmsrop --epochs 40 --batch-size 200 \
     --seed 18092 --cuda --log-interval 200 \
