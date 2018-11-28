e3b3b3b9581b2e55ef2567dc66fba68e942ac2e9
Milton Llera <m.lleramontero@bristol.ac.uk>
2018-11-28 13:02:38 +0000

    Some refactoring

--- benchmarks/seqMNIST/test-scripts/test-run.sh
+++ benchmarks/seqMNIST/test-scripts/test-run.sh
@@ -3,6 +3,6 @@
 cd ..
 source ../../.venv/bin/activate
 
-python run.py --model LSTM --nlayers 1 --nhid 50 \
-    --optim rmsprop --lr 1e-2 --l2-norm 0.1 --epochs 2 --batch-size 50 \
+python run.py --model subLSTM --nlayers 1 --nhid 50 \
+    --optim rmsprop --lr 1e-4 --l2-norm 0.1 --epochs 2 --batch-size 50 \
     --seed 18092 --cuda --log-interval 50 --data MNIST --save results
