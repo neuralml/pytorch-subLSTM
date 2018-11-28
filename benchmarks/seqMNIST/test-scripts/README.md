7797196503beada2093bce6de88f764c1665fa0a
Milton Llera <lleramontero@gmail.com>
2018-11-28 10:21:21 +0000

    Added the bash scripts for different experiments using sequential MNIST.

---
+++ benchmarks/seqMNIST/test-scripts/README.md
@@ -0,0 +1,3 @@
+# Test scripts
+
+I've included a few bash scripts for testing. These assume that they will be run from their containing folder. They will execute the run script in the parent folder with different parameters.
---
+++ benchmarks/seqMNIST/test-scripts/test-lstm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+source ../../.venv/bin/activate
+
+python run.py --model LSTM --nlayers 1 --nhid 50 \
+    --optim rmsprop --lr 10e-4 --l2-norm 0.1 --epochs 40 --batch-size 50 \
+    --seed 18092 --cuda --log-interval 50 --data MNIST --save results
