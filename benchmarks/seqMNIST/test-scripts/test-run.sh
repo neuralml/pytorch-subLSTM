712fffb3b6878156dff393370dfaca2b6118432a
Milton Llera <lleramontero@gmail.com>
2018-11-28 15:48:59 +0000

    Removed keeping track of the hidden states.

--- benchmarks/seqMNIST/test-scripts/test-run.sh
+++ benchmarks/seqMNIST/test-scripts/test-run.sh
@@ -4,5 +4,5 @@ cd ..
 source ../../.venv/bin/activate
 
 python run.py --model subLSTM --nlayers 1 --nhid 50 \
-    --optim rmsprop --lr 1e-4 --l2-norm 0.1 --epochs 2 --batch-size 50 \
+    --optim rmsprop --lr 1e-3 --l2-norm 0.0 --epochs 2 --batch-size 100 \
     --seed 18092 --cuda --log-interval 50 --data MNIST --save results
